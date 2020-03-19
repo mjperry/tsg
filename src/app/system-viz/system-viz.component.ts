76cfeace2feba8587ffd7dc6be81023cc93bccda
Michael Perry <michael.jamal.perry@gmail.com>
2020-03-19 11:29:34 -0500

    Add beginning system graph visualization

---
+++ src/app/system-viz/system-viz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-system-viz',
+  templateUrl: './system-viz.component.html',
+  styleUrls: ['./system-viz.component.css']
+})
+export class SystemVizComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
